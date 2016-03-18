4308123000df4347b4edf3f6e8ebeea1130ef1d4
Aleksander1995 <egor1573@yandex.ru>
2016-03-18 04:48:29 +0300

    New Project2

---
+++ Less1/src/Less1.java
@@ -0,0 +1,8 @@
+
+public class Less1 {
+    public static void main(String...args)
+    {
+        System.out.println("Hello World ");
+        System.out.println("Made by Aleksander Lobov ");
+    }
+}
